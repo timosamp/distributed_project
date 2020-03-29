614e9756acf982e6d342e274e50065093990dcb9
timosamp <timosamp6@gmail.com>
2020-03-29 13:59:49 +0300

    first attempt for writing block

--- block.py
+++ block.py
@@ -1,24 +1,35 @@
-
 import blockchain
 
 import time
+import hashlib as hasher
+
 
 class Block:
-	def __init__(self):
-		##set
+    def __init__(self, index, nonce, listOfTransaction, previous_hash):
 
-		#self.previousHash
+        # Timestamp of block creation
+        self.timestamp = time.time()
 
-		# Timestamp of block creation
-		self.timestamp = time.time()
+        # The hash number of the previous block
+        self.previousHash = previous_hash
 
-		#self.hash
-		#self.nonce
-		#self.listOfTransactions
-	
-	# def myHash:
-		#calculate self.hash
+        self.nonce = nonce
+
+        self.listOfTransactions = listOfTransaction
+
+        self.hash = self.hash_block()
+
+        def hash_block():
+            # calculate self.hash
+            sha = hasher.sha256()
+            sha.update(str(self.index) +
+                       str(self.timestamp) +
+                       str(self.data) +
+                       str(self.previous_hash))
+            return sha.hexdigest()
+
+# def myHash:
 
 
-	# def add_transaction(transaction transaction, blockchain blockchain):
-		#add a transaction to the block
+# def add_transaction(transaction transaction, blockchain blockchain):
+# add a transaction to the block
